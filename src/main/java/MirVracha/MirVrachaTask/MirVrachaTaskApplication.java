f9a0095fdcb7f66095c53fc742f4b76669f4106e
Artur <archiesummerrain@gmail.com>
2021-02-08 20:52:16 +0500

    Initial commit

---
+++ src/main/java/MirVracha/MirVrachaTask/MirVrachaTaskApplication.java
@@ -0,0 +1,13 @@
+package MirVracha.MirVrachaTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirVrachaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirVrachaTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MirVracha/MirVrachaTask/MirVrachaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package MirVracha.MirVrachaTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirVrachaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
